4946d7dfe669451a4f68cf4fa0f1497baf4ee8c5
OliKirk <owk0907@gmail.com>
2023-05-09 13:32:29 +0200

    SPA VIRKER

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,10 @@
 "use strict";
 import { getMembers } from "./rest-service.js";
+import { initViews } from "./view-spa.js";
+
 window.addEventListener("load", initApp);
 function initApp() {
+  initViews();
   getMembers();
   console.log(getMembers());
 }
