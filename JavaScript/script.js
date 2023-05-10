eb1fcd605933c9132d3a872d9446619b8d6adbb0
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2023-05-10 12:55:29 +0200

    Made an age calculator

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,22 @@
 "use strict";
 
-var app = angular.module("myAPP", ["ngRoute"]);
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when("/", { templateURL: "main.htm" })
-    .when("/nav1", { templateURL: "nav1.htm" })
-    .when("/nav2", { templateURL: "nav2.htm" });
-});
+var dob = new Date("09/08/1994");
+//calculate month difference from current date in time
+var month_diff = Date.now() - dob.getTime();
+
+//convert the calculated difference in date format
+var age_dt = new Date(month_diff);
+
+//extract year from date
+var year = age_dt.getUTCFullYear();
+
+//now calculate the age of the user
+var age = Math.abs(year - 1970);
+
+//display the calculated age
+
+console.log(age);
+
+document
+  .querySelector("#h1")
+  .insertAdjacentHTML("beforeend", `You are ${age} years old.`);
