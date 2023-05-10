34ee6038d11886b6ce2bd6d9863c48086dac8de9
LauritsSchou <laurits.schou@gmail.com>
2023-05-10 13:37:01 +0200

    converted to a function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,22 +1,28 @@
 "use strict";
 
-var dob = new Date("09/08/1994");
-//calculate month difference from current date in time
-var month_diff = Date.now() - dob.getTime();
+// var dob = new Date("1995-08-06");
+// //calculate month difference from current date in time
+// var month_diff = Date.now() - dob.getTime();
 
-//convert the calculated difference in date format
-var age_dt = new Date(month_diff);
+// //convert the calculated difference in date format
+// var age_dt = new Date(month_diff);
 
-//extract year from date
-var year = age_dt.getUTCFullYear();
+// //extract year from date
+// var year = age_dt.getUTCFullYear();
 
-//now calculate the age of the user
-var age = Math.abs(year - 1970);
+// //now calculate the age of the user
+// var age = Math.abs(year - 1970);
 
-//display the calculated age
+// //display the calculated age
 
-console.log(age);
-
-document
-  .querySelector("#h1")
-  .insertAdjacentHTML("beforeend", `You are ${age} years old.`);
+// console.log(age);
+ageCalculator();
+function ageCalculator() {
+  let dob = new Date("1995-08-06");
+  let monthDiff = Date.now() - dob.getTime();
+  let ageDateFormat = new Date(monthDiff);
+  let year = ageDateFormat.getUTCFullYear();
+  let age = Math.abs(year - 1970);
+  console.log(age);
+  document.querySelector("#h1").insertAdjacentHTML("beforeend", `You are ${age} years old.`);
+}
