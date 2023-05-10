52339fef4ff967108971dd30d6b339c9f865da04
LauritsSchou <laurits.schou@gmail.com>
2023-05-10 13:16:00 +0200

    filterResultsByDiscipline function done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,13 +2,19 @@
 import { getMembers, getResults, createMember, createResult } from "./rest-service.js";
 window.addEventListener("load", initApp);
 let members;
+let results;
 async function initApp() {
   await showMembers();
+  await showResults();
   await filterMembersByPayed(members);
   await filterMembersByNotPayed(members);
+  filterResultsByDiscipline("crawl");
 }
 async function showMembers() {
   members = await getMembers();
+}
+async function showResults() {
+  results = await getResults();
 }
 async function filterMembersByPayed(members) {
   const result = members.filter((member) => member.hasPayed === true);
@@ -17,4 +23,13 @@ async function filterMembersByPayed(members) {
 async function filterMembersByNotPayed(members) {
   const result = members.filter((member) => member.hasPayed !== true);
   console.log(result);
+}
+function filterResultsByDiscipline(searchValue) {
+  searchValue = searchValue.toLowerCase();
+  const result = results.filter(matchDiscipline);
+  function matchDiscipline(result) {
+    return result.discipline.toLowerCase().includes(searchValue);
+  }
+  console.log(result);
+  return result;
 }
