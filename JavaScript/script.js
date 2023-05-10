91a8e2fbcd1efe311f004a05f7d73fa8f05c5dd9
LauritsSchou <laurits.schou@gmail.com>
2023-05-10 12:00:28 +0200

    sort by type, time and discipline

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,29 @@
 "use strict";
-import { getMembers, getResults, createMember, createResult } from "./rest-service.js";
+import { getMembers, getResults, createMember, createResult, deleteMember } from "./rest-service.js";
+let members;
+let results;
 window.addEventListener("load", initApp);
-function initApp() {
-  getMembers();
-  console.log(getMembers());
-  console.log(getResults());
-  
+async function initApp() {
+  await showResults();
+  await sortResultsByType();
+  await sortResultsByTime();
+  await sortResultsByDiscipline();
+}
+async function showResults() {
+  results = await getResults();
+}
+async function sortResultsByType() {
+  await showResults();
+  results.sort((a, b) => a.type.localeCompare(b.type));
+  console.log(results);
+}
+async function sortResultsByTime() {
+  await showResults();
+  results.sort((a, b) => a.time - b.time);
+  console.log(results);
+}
+async function sortResultsByDiscipline() {
+  await showResults();
+  results.sort((a, b) => a.discipline.localeCompare(b.discipline));
+  console.log(results);
 }
