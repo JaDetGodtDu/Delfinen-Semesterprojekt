e1c3887d7d88d30376a9e1dcd234c0dfde1281d6
LauritsSchou <laurits.schou@gmail.com>
2023-05-10 12:42:05 +0200

    filter by hasPayed working

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,11 +5,16 @@ let members;
 async function initApp() {
   await showMembers();
   await filterMembersByPayed(members);
+  await filterMembersByNotPayed(members);
 }
 async function showMembers() {
   members = await getMembers();
 }
 async function filterMembersByPayed(members) {
-  const result = members.filter("hasPayed" === true);
+  const result = members.filter((member) => member.hasPayed === true);
+  console.log(result);
+}
+async function filterMembersByNotPayed(members) {
+  const result = members.filter((member) => member.hasPayed !== true);
   console.log(result);
 }
