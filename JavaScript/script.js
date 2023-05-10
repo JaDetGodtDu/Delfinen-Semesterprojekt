9394e96be5d1f5837df4cd230e83d403dc6c3da0
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2023-05-10 12:39:18 +0200

    Trying angular routing

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var app = angular.module("myAPP", ["ngRoute"]);
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", { templateURL: "main.htm" })
+    .when("/nav1", { templateURL: "nav1.htm" })
+    .when("/nav2", { templateURL: "nav2.htm" });
+});
