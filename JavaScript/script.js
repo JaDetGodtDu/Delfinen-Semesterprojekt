b8f7519e715c607a8a0e1fd26846188fe7705061
LauritsSchou <laurits.schou@gmail.com>
2023-05-10 10:37:57 +0200

    Træner form og funktion implementeret

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,19 @@
 "use strict";
+window.addEventListener("load", initApp);
+function initApp() {
+  document.querySelector("#type").addEventListener("change", (event) => competitionTypeChange(event));
+}
+function competitionTypeChange(event) {
+  const selectedTargetValue = event.target.value;
+  if (selectedTargetValue === "competition") {
+    document.querySelector("#competition-name").removeAttribute("hidden");
+    document.querySelector("#competition-name-label").removeAttribute("hidden");
+    document.querySelector("#placement-label").removeAttribute("hidden");
+    document.querySelector("#placement").removeAttribute("hidden");
+  } else {
+    document.querySelector("#competition-name").setAttribute("hidden", "true");
+    document.querySelector("#competition-name-label").setAttribute("hidden", "true");
+    document.querySelector("#placement-label").setAttribute("hidden", "true");
+    document.querySelector("#placement").setAttribute("hidden", "true");
+  }
+}
