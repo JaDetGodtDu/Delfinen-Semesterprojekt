70f879077463f275c80d9e4977a5d4831da1fba9
LauritsSchou <laurits.schou@gmail.com>
2023-05-15 13:21:41 +0200

    CreateMember function working

--- JavaScript/formand.js
+++ JavaScript/formand.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { memberAgegroup, compSwimmer, subscriptionType } from "./helpers.js";
-import { getMembers } from "./rest-service.js";
+import { getMembers, createMember } from "./rest-service.js";
 
 async function updateMemberTable() {
   let members = await getMembers();
@@ -9,6 +9,7 @@ async function updateMemberTable() {
 }
 
 function showMembers(members) {
+  document.querySelector("#formand-table-body").innerHTML = "";
   for (const member of members) {
     showTable(member);
   }
@@ -24,12 +25,27 @@ function showTable(member) {
       <td>${subscriptionType(member)}</td>
     </tr>
     `;
-  document
-    .querySelector("#formand-table-body")
-    .insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
+  document.querySelector("#formand-table-body").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
 }
 function showCreateMemberDialog() {
   document.querySelector("#create-member-dialog").showModal();
+  document.querySelector("#create-member-form").addEventListener("submit", prepareNewMemberData);
 }
 
+async function prepareNewMemberData() {
+  const firstName = document.querySelector("#firstName").value;
+  const lastName = document.querySelector("#lastName").value;
+  const address = document.querySelector("#address").value;
+  const phone = document.querySelector("#phone").value;
+  const email = document.querySelector("#email").value;
+  const dateOfBirth = document.querySelector("#dateOfBirth").value;
+  const gender = document.querySelector("#gender").value;
+  const active = document.querySelector("#active").value;
+  const compSwimmer = document.querySelector("#compSwimmer").value;
+  const response = await createMember(firstName, lastName, address, phone, email, dateOfBirth, gender, active, compSwimmer);
+  if (response.ok) {
+    updateMemberTable();
+    document.querySelector("#create-member-dialog").close();
+  }
+}
 export { showMembers, showCreateMemberDialog };
