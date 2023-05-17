73073ea540f011df700159853f173180a1d38ada
HAlbergson <hjalte3480@gmail.com>
2023-05-17 11:28:45 +0200

    search i senior

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,25 +2,16 @@
 
 import { showCreateMemberDialog, searchMembersFormand } from "./formand.js";
 import { searchMembersKassér } from "./kassér.js";
-import {
-  getMembers,
-  getResults,
-  createMember,
-  createResult,
-} from "./rest-service.js";
+import { searchMembersSenior } from "./senior.js";
+import { getMembers, getResults, createMember, createResult } from "./rest-service.js";
 window.addEventListener("load", initApp);
 
 function initApp() {
   getMembers();
   console.log(getMembers());
   console.log(getResults());
-  document
-    .querySelector("#input-search-kassér")
-    .addEventListener("keydown", searchMembersKassér);
-  document
-    .querySelector("#input-search-formand")
-    .addEventListener("keydown", searchMembersFormand);
-  document
-    .querySelector("#create-new-member-btn")
-    .addEventListener("click", showCreateMemberDialog);
+  document.querySelector("#input-search-kassér").addEventListener("keydown", searchMembersKassér);
+  document.querySelector("#input-search-formand").addEventListener("keydown", searchMembersFormand);
+  document.querySelector("#input-search-senior").addEventListener("keydown", searchMembersSenior);
+  document.querySelector("#create-new-member-btn").addEventListener("click", showCreateMemberDialog);
 }
--- JavaScript/senior.js
+++ JavaScript/senior.js
@@ -35,4 +35,36 @@ function showSeniorTable(result) {
     `;
   document.querySelector("#senior-table-body").insertAdjacentHTML("beforeend", seniorHTML);
 }
-export { seniorShowMembers };
+
+function searchMembersSenior() {
+  let searchInput = document.getElementById("input-search-senior");
+  let table = document.getElementById("senior-table-body");
+
+  // Add an event listener to the input field
+  searchInput.addEventListener("input", function () {
+    let filter = searchInput.value.toUpperCase();
+    let rows = table.getElementsByTagName("tr");
+
+    // Loop through the table rows and hide those that don't match the filter
+    for (let i = 0; i < rows.length; i++) {
+      let cells = rows[i].getElementsByTagName("td");
+      let shouldHide = true;
+
+      // Loop through the cells of each row
+      for (let j = 0; j < cells.length; j++) {
+        let cell = cells[j];
+        if (cell) {
+          let cellText = cell.textContent || cell.innerText;
+          if (cellText.toUpperCase().indexOf(filter) > -1) {
+            shouldHide = false;
+            break;
+          }
+        }
+      }
+
+      // Toggle the display property based on the filter condition
+      rows[i].style.display = shouldHide ? "none" : "";
+    }
+  });
+}
+export { seniorShowMembers, searchMembersSenior };
