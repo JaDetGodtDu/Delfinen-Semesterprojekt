a4af8c855199ef1e18620c49a9b2222727c5c7ae
HAlbergson <hjalte3480@gmail.com>
2023-05-24 11:07:17 +0200

    implementeret på alle dialog og forms

--- JavaScript/formand.js
+++ JavaScript/formand.js
@@ -1,44 +1,19 @@
 "use strict";
 
-import {
-  memberAgeGroup,
-  compSwimmer,
-  subscriptionType,
-  memberGender,
-  ageCalculator,
-} from "./helpers.js";
-import {
-  getMembers,
-  createMember,
-  deleteMember,
-  updateMember,
-} from "./rest-service.js";
+import { memberAgeGroup, compSwimmer, subscriptionType, memberGender, ageCalculator } from "./helpers.js";
+import { getMembers, createMember, deleteMember, updateMember } from "./rest-service.js";
 
 window.addEventListener("load", initApp);
 
 function initApp() {
   updateMemberTable();
-  document
-    .querySelector("#form-delete-member")
-    .addEventListener("submit", deleteMemberClicked);
-  document
-    .querySelector("#form-delete-member .btn-cancel")
-    .addEventListener("click", deleteCancelClicked);
-  document
-    .querySelector("#form-update-member")
-    .addEventListener("submit", updateMemberClicked);
-  document
-    .querySelector("#form-update-member .btn-cancel")
-    .addEventListener("click", updateCancelClicked);
-  document
-    .querySelector("#formand-select-filter-by")
-    .addEventListener("change", filterByChanged);
-  document
-    .querySelector("#create-new-member-btn")
-    .addEventListener("click", showCreateMemberDialog);
-  document
-    .querySelector("#create-member-form .btn-cancel")
-    .addEventListener("click", createMemberCancel);
+  document.querySelector("#form-delete-member").addEventListener("submit", deleteMemberClicked);
+  document.querySelector("#form-delete-member .btn-cancel").addEventListener("click", deleteCancelClicked);
+  document.querySelector("#form-update-member").addEventListener("submit", updateMemberClicked);
+  document.querySelector("#form-update-member .btn-cancel").addEventListener("click", updateCancelClicked);
+  document.querySelector("#formand-select-filter-by").addEventListener("change", filterByChanged);
+  document.querySelector("#create-new-member-btn").addEventListener("click", showCreateMemberDialog);
+  document.querySelector("#create-member-form .btn-cancel").addEventListener("click", createMemberCancel);
 }
 async function updateMemberTable() {
   let members = await getMembers();
@@ -53,9 +28,7 @@ function showMembers(members) {
 function showTable(member) {
   const html = /*html*/ `
     <tr>
-      <td style="color:blue" class="name"><u>${member.firstName} ${
-    member.lastName
-  }</u></td>
+      <td style="color:blue" class="name"><u>${member.firstName} ${member.lastName}</u></td>
       <td>${memberAgeGroup(member)}</td>
       <td>${compSwimmer(member)}</td>
       <td>${member.email}</td>
@@ -63,9 +36,7 @@ function showTable(member) {
       <td>${subscriptionType(member)}</td>
     </tr>
     `;
-  document
-    .querySelector("#formand-table-body")
-    .insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
+  document.querySelector("#formand-table-body").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
   const rows = document.querySelectorAll("#formand-table-body tr");
   const lastRow = rows[rows.length - 1];
   lastRow.addEventListener("click", () => memberClicked(member));
@@ -84,19 +55,14 @@ function memberClicked(member) {
   <input type="button" value="Opdater medlem" button id="update-member-btn">
   <input type="button" value="Slet medlem" button id="delete-member-btn">
   <input type="button" value="Tilbage" button id="member-clicked-cancel-btn">
+
  
   `;
   document.querySelector("#member-detail-view").innerHTML = memberInfo;
   document.querySelector("#member-detail-view").showModal();
-  document
-    .querySelector("#delete-member-btn")
-    .addEventListener("click", () => deleteClicked(member));
-  document
-    .querySelector("#update-member-btn")
-    .addEventListener("click", () => updateClicked(member));
-  document
-    .querySelector("#member-clicked-cancel-btn")
-    .addEventListener("click", memberClickedCancel);
+  document.querySelector("#delete-member-btn").addEventListener("click", () => deleteClicked(member));
+  document.querySelector("#update-member-btn").addEventListener("click", () => updateClicked(member));
+  document.querySelector("#member-clicked-cancel-btn").addEventListener("click", memberClickedCancel);
 }
 function createMemberCancel() {
   document.querySelector("#create-member-dialog").close();
@@ -106,16 +72,11 @@ function memberClickedCancel() {
 }
 function showCreateMemberDialog() {
   document.querySelector("#create-member-dialog").showModal();
-  document
-    .querySelector("#create-member-form")
-    .addEventListener("submit", prepareNewMemberData);
+  document.querySelector("#create-member-form").addEventListener("submit", prepareNewMemberData);
 }
 function deleteClicked(memberObject) {
-  document.querySelector("#dialog-delete-member-name").textContent =
-    memberObject.firstName + " " + memberObject.lastName;
-  document
-    .querySelector("#form-delete-member")
-    .setAttribute("data-id", memberObject.id);
+  document.querySelector("#dialog-delete-member-name").textContent = memberObject.firstName + " " + memberObject.lastName;
+  document.querySelector("#form-delete-member").setAttribute("data-id", memberObject.id);
   document.querySelector("#dialog-delete-member").showModal();
 }
 async function deleteMemberClicked(event) {
@@ -160,18 +121,7 @@ async function updateMemberClicked(event) {
   const compSwimmer = form.compSwimmer.value;
   const id = form.getAttribute("data-id");
 
-  const response = await updateMember(
-    id,
-    firstName,
-    lastName,
-    address,
-    phone,
-    email,
-    compSwimmer,
-    active,
-    gender,
-    dateOfBirth
-  );
+  const response = await updateMember(id, firstName, lastName, address, phone, email, compSwimmer, active, gender, dateOfBirth);
   document.querySelector("#member-detail-view").close();
   if (response.ok) {
     updateMemberTable();
@@ -187,17 +137,7 @@ async function prepareNewMemberData() {
   const gender = document.querySelector("#gender").value;
   const active = document.querySelector("#active").value;
   const compSwimmer = document.querySelector("#compSwimmer").value;
-  const response = await createMember(
-    firstName,
-    lastName,
-    address,
-    phone,
-    email,
-    dateOfBirth,
-    gender,
-    active,
-    compSwimmer
-  );
+  const response = await createMember(firstName, lastName, address, phone, email, dateOfBirth, gender, active, compSwimmer);
   if (response.ok) {
     updateMemberTable();
     document.querySelector("#create-member-dialog").close();
@@ -215,9 +155,7 @@ async function filterByChanged() {
   } else if (filterValue === "senior") {
     results = members.filter((member) => ageCalculator(member) >= 18);
   } else if (filterValue.startsWith("!")) {
-    results = members.filter(
-      (member) => member[filterValue.substring(1)] === "false"
-    );
+    results = members.filter((member) => member[filterValue.substring(1)] === "false");
   } else if (filterValue === "showAll") {
     results = members;
   } else {
